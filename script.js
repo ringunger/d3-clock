80c7c312f4900b21e5227f07d9ac811ee763896f
Ringle <=>
2019-02-02 13:19:12 +0300

    Final stage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
     var width = 300;
     var height = 400;
-    var radius = 100;
+    var radius = 120;
 function drawCanvas(){
     var center_x = width / 2;
     var center_y = height / 2;
@@ -38,24 +38,24 @@ function drawCanvas(){
         .attr("stroke-width", 6).attr("stroke", "#eee").attr("fill", "url(#clock-bg)")
         .style("filter", "url(#dropshadow)");
 
-    var angle = 0; var font_size = 18;
+    var angle = 0; var font_size = 16;
     for(var i = 0; i < 60; i++){
         var tick_l = 8;
         if(i % 5) {tick_l = 4;}
         svg.append("line")
         .attr("class", ((i % 5) == 0) ? "hour" : "minute" + " tick")
         .attr("x1", center_x)
-        .attr("y1", radius + 1)
+        .attr("y1", (center_y - radius) + 1)
         .attr("x2", center_x)
-        .attr("y2", radius  +  tick_l)
+        .attr("y2", (center_y - radius)  +  tick_l)
         .style("stroke", "black")
         .attr("transform", "rotate(" + i * 6 + "," + center_x + "," + center_y + ")");
 
         if(i % 5 == 0) {
             svg.append("text")
             .attr("class", "clock-text")
-            .attr("x", center_x - (font_size / 2))
-            .attr("y", radius +  font_size + tick_l + 2)
+            .attr("x", (i >= 50 || i == 0) ? (center_x - (font_size / 2)) : (center_x -(font_size / 4)))
+            .attr("y", (center_y - radius) +  font_size + tick_l + 2)
             .attr("transform", "rotate(" + i * 6 + "," + center_x + "," + center_y + ")")
             .attr("font-size", font_size + "px")
             .attr("color", "black")
@@ -63,21 +63,21 @@ function drawCanvas(){
         }
     }
 
-    svg.append("text").attr("class", "clock-logo").attr("x", center_x - 26).attr("y", center_y + (radius / 2))
+    svg.append("text").attr("class", "clock-logo").attr("x", center_x - 32).attr("y", center_y + (radius / 2))
                         .style("fill", "#ddd")
                         .text("RingleSoft");
 
     svg.append("line").attr("class", "arrow second")
     .attr("x1", center_x).attr("y1", center_y + 12)
-    .attr("x2", center_x).attr("y2", (radius + (0.2 * radius)));
+    .attr("x2", center_x).attr("y2", ((center_y - radius) + (0.2 * radius)));
 
     svg.append("line").attr("class", "arrow minute")
     .attr("x1", center_x).attr("y1", center_y + 12)
-    .attr("x2", center_x).attr("y2", (radius + (0.2 * radius)));
+    .attr("x2", center_x).attr("y2", ((center_y - radius) + (0.3 * radius)));
 
     svg.append("line").attr("class", "arrow hour")
     .attr("x1", center_x).attr("y1", center_y + 12)
-    .attr("x2", center_x).attr("y2", (radius + (0.45 * radius)));
+    .attr("x2", center_x).attr("y2", ((center_y - radius) + (0.45 * radius)));
 
     svg.append("circle").attr("cx", center_x).attr("cy", center_y).attr("r", 6)
         .style("fill", "white").style("stroke", "black").style("strokeWidth", 4);
